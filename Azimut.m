f1ac4b19185e43dba3854a451041bb0c8f12e15b
H-Manuel <hm2799@mci4me.at>
2025-01-08 15:39:04 +0100

    functions with plots

--- Azimut.m
+++ Azimut.m
@@ -1,8 +1,8 @@
-function [az] = Azimut(delta, a, phi)
+function [az] = Azimut(delta, a, phi,t)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
 cosaz=(sind(delta)-sind(a)*sind(phi))./(cosd(a)*cosd(phi));
 az=real(acosd(cosaz)); % Azimut az (Winkel zur Nordrichtung)
-
+plot(t,az)
 end
--- Hoehenwinkel.m
+++ Hoehenwinkel.m
@@ -3,4 +3,7 @@ function a = Hoehenwinkel(phi, delta, t)
 %   Detailed explanation goes here
 sin_a=@(t) (sind(phi)*sind(delta)+cosd(phi)*cosd(delta)*cosd(15*(t-12)));
 a = asind(sin_a(t)); % Höhenwinkel alpha
+plot(t,a)
+ylim([-50 50]);
+xlim([0 24]);
 end
