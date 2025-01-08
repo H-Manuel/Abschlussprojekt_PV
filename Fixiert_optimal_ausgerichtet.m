4dc586a520057d9d19cc00c8c162e8c81b78aabc
oubi-aed <alexander.oberhoer23@gmail.com>
2025-01-08 15:15:14 +0100

    Vorlagen Funktionen

--- Fixiert_optimal_ausgerichtet.m
+++ Fixiert_optimal_ausgerichtet.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2] = untitled(inputArg1,inputArg2)
+function [outputArg1,outputArg2] = Fixiert_optimal_ausgerichtet(inputArg1,inputArg2)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 outputArg1 = inputArg1;
