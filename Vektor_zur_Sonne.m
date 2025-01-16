e4568eb26620a8be1f181e83787f78d76ef4a6d8
LeoMaier1 <ml9691@mci4me.at>
2025-01-16 10:24:57 +0100

    funktion vektor zur sonne geschrieben

--- Vektor_zur_Sonne.m
+++ Vektor_zur_Sonne.m
@@ -1,6 +1,15 @@
-function [outputArg1,outputArg2] = Vektor_zur_Sonne(inputArg1,inputArg2)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function sonnen_vektor = Vektor_zur_Sonne(breite, doy, zeit)
+    % Umwandlung des DOY (Tag des Jahres) in ein datetime-Objekt
+    datum = datetime(2025, 1, 1) + days(doy - 1);  % Jahr 2025, mit DOY als Tag des Jahres
+    
+    % Berechnungen der Deklination, Stundenwinkel, Höhenwinkel und Azimut
+    delta = Deklination(datum); % Deklination (mit datetime)
+    hoehenwinkel = Hoehenwinkel(breite, delta, zeit); % Höhenwinkel
+    azimutwinkel = Azimut(delta, hoehenwinkel, breite, zeit); % Azimutwinkel
+    
+    % Berechnung des Sonnenvektors
+    sx = cosd(hoehenwinkel) .* cosd(azimutwinkel);
+    sy = cosd(hoehenwinkel) .* sind(azimutwinkel);
+    sz = sind(hoehenwinkel);
+    sonnen_vektor = [sx, sy, sz];
 end
